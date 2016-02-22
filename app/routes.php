3bdca69fa1e50857461771aa89359ba2825b40a0
Benali Abdessamad <Benali Abdessamad>
2016-02-22 11:41:02 +0100

    Iteration 4: use Doctrine

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,11 @@
 <?php
 
 // Home page
-$app->get('/', function () {
-    require '../src/model.php';
-    $articles = getArticles();
+$app->get('/', function () use ($app) {
+    $articles = $app['dao.article']->findAll();
 
     ob_start();             // start buffering HTML output
-    require '../views/views.php';
+    require '../views/view.php';
     $view = ob_get_clean(); // assign HTML output to $view
     return $view;
 });
--- src/model.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function getArticles() {
-    $bdd = new PDO('mysql:host=localhost;dbname=microcms;charset=utf8', 'microcms_user', 'secret');
-    return $bdd->query('select * from t_article order by art_id desc');
-}
